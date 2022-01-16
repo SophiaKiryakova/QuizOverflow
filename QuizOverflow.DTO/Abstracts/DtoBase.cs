1e3c9775b0c13d64b16357b70f7b318b720535ed
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2022-01-16 16:10:31 +0200

    QO8 - Added dtos, automapper and mapping profiles.

---
+++ QuizOverflow.DTO/Abstracts/DtoBase.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizOverflow.DTO
+{
+    public class DtoBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ QuizOverflow.DTO/QuestionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizOverflow.DTO
+{
+    public class QuestionDto : DtoBase
+    {
+        public string? Name { get; set; }
+        public int Points { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<AnswerDto>? Answers { get; set; }
+    }
+}
