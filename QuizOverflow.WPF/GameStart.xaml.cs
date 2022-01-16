d92e47c818df0ba01ceea8ea5c9dd5f103d9e7a1
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2022-01-16 19:20:28 +0200

    QO9 - Add PresentationLayer with WPF initial project.

---
+++ QuizOverflow.WPF/GameStart.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace QuizOverflow.WPF
+{
+    /// <summary>
+    /// Interaction logic for GameStart.xaml
+    /// </summary>
+    public partial class GameStart : Page
+    {
+        public GameStart()
+        {
+            InitializeComponent();
+        }
+    }
+}
