708c842e38956e066530df259dca6731041f1e71
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2022-01-16 19:22:19 +0200

    QO9 - Add PresentationLayer with WPF initial project. (#6)

---
+++ QuizOverflow.WPF/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using QuizOverflow.Data;
+using QuizOverflow.Data.Contracts;
+using QuizOverflow.Services.Contracts;
+using QuizOverflow.Services.MappingProfiles;
+using QuizOverflow.Services.Services;
+using System;
+using System.Windows;
+
+namespace QuizOverflow.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceProvider = CreateServiceProvider();
+            serviceProvider.GetRequiredService<MainWindow>();
+
+            base.OnStartup(e);
+        }
+
+        private IServiceProvider CreateServiceProvider()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddScoped<ISeedService, SeedService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IQuizOverflowDbContext, QuizOverflowDbContext>();
+
+
+            IConfigurationProvider config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, Mapper>();
+
+            services.AddSingleton<MainWindow>(s => new MainWindow(s.GetRequiredService<ISeedService>()));
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ QuizOverflow.WPF/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using QuizOverflow.Services.Contracts;
+using System.Windows.Navigation;
+
+namespace QuizOverflow.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow(ISeedService seedService)
+        {
+            seedService.SeedCategories();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
