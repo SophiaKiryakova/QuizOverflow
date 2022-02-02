8db1d3967c2b7462bc45a14a756cdc3dc2addbce
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2022-02-02 10:36:18 +0200

    QO2 - Createed Game Entity and added migration. Added it to the UnitOfWork.

---
+++ QuizOverflow.Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using QuizOverflow.Models.Abstracts;
+
+namespace QuizOverflow.Models
+{
+    public class Game : EntityBase
+    {
+        public DateTime DatePlayedOn { get; set; }
+        List<GamePlayer> GamesPlayers { get; set; }
+        List<GameQuestion> GamesQuestions { get; set; }
+    }
+}
---
+++ QuizOverflow.Models/GamePlayer.cs
@@ -0,0 +1,13 @@
+﻿using QuizOverflow.Models.Abstracts;
+
+namespace QuizOverflow.Models
+{
+    public class GamePlayer : EntityBase
+    {
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+    }
+}
